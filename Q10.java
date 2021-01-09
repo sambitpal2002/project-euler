e3eaef798523da4bc6187b0daf79ff110deb5a5a
Sambit Sourav Pal <53575158+sambitpal2002@users.noreply.github.com>
2021-01-09 09:04:38 +0530

    Update Q10.java

--- Q10.java
+++ Q10.java
@@ -8,28 +8,31 @@ public class Q10 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		long i,num,count;
-		long sum=0;
-		
-		for (num=2;num<2000000;num++) 
-		{
-			count=0;
-			for (i=2;i<num/2;i++) 
-			{
-				if(num%i==0) {
-					count++;
-					break;
-				}
-			}
-			if(count==0 && num!=1) {
-				sum=sum+num;
-			}
-		
-		}
-		System.out.println("The Sum of Prime Number below 2Million = "+sum);
+		 long sum = 0;
+
+	    for (int i = 2; i < 2000000; i++) {
+	        boolean isPrime = true;
+
+	        if ((i % 2 == 0 || i % 3 == 0) && (i!=2 || i!=3 || i!=5)) {
+	            isPrime = false;
+	        } else {
+	            for (int j = 5; j <= Math.sqrt(i); j = j + 6) {
+	                if (i % j == 0 || i % (j + 2) == 0) {
+	                    isPrime = false;
+	                    break;
+	                }
+	            }
+	        }
+
+	        if (isPrime == true) {
+	            sum += i;
+	        }
+	    }
+
+	    System.out.println(sum);
 
 	}
 
 }
 
-//OUTPUT: The Sum of Prime Number below 2Million = 142913828926
+//OUTPUT:142913828926
