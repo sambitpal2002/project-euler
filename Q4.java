dd5991480ba92ebc89dac07d49a49479631c342f
Sambit Sourav Pal <53575158+sambitpal2002@users.noreply.github.com>
2021-01-09 09:13:42 +0530

    Update Q4.java

--- Q4.java
+++ Q4.java
@@ -1,4 +1,7 @@
+/*A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
+Find the largest palindrome made from the product of two 3-digit numbers.
+*/
 public class Q4 {
 
 	public static void main(String[] args) {
