befb91a27ffb3d96ade22d3826e37abd351e7f01
Sambit Sourav Pal <53575158+sambitpal2002@users.noreply.github.com>
2021-01-14 09:25:22 +0530

    Update Q5.java

--- Q5.java
+++ Q5.java
@@ -5,27 +5,34 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
  */
 
-
-
-
 public class Q5 {
+	private static boolean div(int a) {
+		int b=0;
+	    for(int i=1;i<=20;i++) 
+	{
+		if(a%i==0) {
+			b++;
+		}
+	}
+	if(b==20)
+		return true;
+	else 
+		return false;
+}
+
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int i = 1;
-		 
-		while (i %  2 != 0 || i %  3 != 0 || i %  4 != 0 || i %  5 != 0 ||
-		         i %  6 != 0 || i %  7 != 0 || i %  8 != 0 || i %  9 != 0 ||
-		         i % 10 != 0 || i % 11 != 0 || i % 12 != 0 || i % 13 != 0 ||
-		         i % 14 != 0 || i % 15 != 0 || i % 16 != 0 || i % 17 != 0 ||
-		         i % 18 != 0 || i % 19 != 0 || i % 20 != 0 ){
-		    i++;
+		for(int i=20;i>=1;i+=20) {
+			if(div(i)) {
+				System.out.println(i);
+				break;
+			}
+
+	
 		}
-		
-		System.out.println("Smallest positive no divisible by 1-20 : "+i);
-
 	}
-
 }
 //OUTPUT:232792560
